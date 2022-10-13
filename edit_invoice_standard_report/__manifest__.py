647d861c1065fd70b5d4f7df90bc84828dbd8e5e
Marwa <marwa1995393@gmail.com>
2022-10-13 18:07:23 +0200

    [add]

---
+++ edit_invoice_standard_report/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'edit invoice standard report',
+    'author': "Marwa Ahmed",
+    'depends': ['base','account','sale_stock' ],
+    'data': [
+             'views/templates.xml',
+
+             ],
+    'installable': True,
+    'application': True,
+}
