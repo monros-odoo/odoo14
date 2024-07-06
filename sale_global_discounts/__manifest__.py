dc74c839aba6d555e0cdc632bf413dc1af1192df
nithin <nithin@flintelli.com>
2024-07-06 15:40:57 +0530

    Revert "9823"
    This reverts commit 3b32f106b1f6c0cb36ccb40b8045e7f5f14aef01.

--- sale_global_discounts/__manifest__.py
+++ sale_global_discounts/__manifest__.py
@@ -18,8 +18,6 @@
     'depends': ['sale','account'],
     'data': [
         'views/sale_view.xml',
-        'views/account_invoice.xml',
-        'views/sale_order_report.xml',
         'views/res_config_view.xml',
     ],
     'images': [
--- sale_global_discounts/models/__init__.py
+++ sale_global_discounts/models/__init__.py
@@ -2,5 +2,5 @@
 
 from . import sale
 from . import res_config
-from .import account_invoice
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
