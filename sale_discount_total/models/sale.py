7e2b140b163bf2d79c773104d629433a7b2e7608
nithin <nithin@flintelli.com>
2024-07-06 15:40:35 +0530

    Revert "9823"
    This reverts commit b4b8654a832e652634558854014624cd01a580f1.

--- sale_discount_total/models/sale.py
+++ sale_discount_total/models/sale.py
@@ -27,6 +27,23 @@ import odoo.addons.decimal_precision as dp
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = amount_discount = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+                amount_discount += (line.product_uom_qty * line.price_unit * line.discount) / 100
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_discount': amount_discount,
+                'amount_total': amount_untaxed + amount_tax,
+            })
 
     discount_type = fields.Selection([('percent', 'Percentage'), ('amount', 'Amount')], string='Discount type',
                                      readonly=True,
@@ -34,18 +51,32 @@ class SaleOrder(models.Model):
                                      default='percent')
     discount_rate = fields.Float('Discount Rate', digits=dp.get_precision('Account'),
                                  readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
+                                     track_visibility='always')
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
+                                 track_visibility='always')
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
+                                   track_visibility='always')
+    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_amount_all',
+                                      digits=dp.get_precision('Account'), track_visibility='always')
 
+    @api.onchange('discount_type', 'discount_rate', 'order_line')
+    def supply_rate(self):
 
-    # @api.onchange('discount_type', 'discount_rate', 'order_line')
-    # def supply_rate(self):
-    #
-    #     for order in self:
-    #         if order.discount_type == 'percent':
-    #             order.amount_discount = (order.amount_untaxed * order.discount_rate) / 100
-    #             order.amount_total = order.amount_untaxed + order.amount_discount
-    #         else:
-    #             order.amount_discount = order.discount_rate
-    #             order.amount_total = order.amount_untaxed + order.amount_discount
+        for order in self:
+            if order.discount_type == 'percent':
+                for line in order.order_line:
+                    line.discount = order.discount_rate
+            else:
+                total = discount = 0.0
+                for line in order.order_line:
+                    total += round((line.product_uom_qty * line.price_unit))
+                if order.discount_rate != 0:
+                    discount = (order.discount_rate / total) * 100
+                else:
+                    discount = order.discount_rate
+                for line in order.order_line:
+                    line.discount = discount
 
     def _prepare_invoice(self, ):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
