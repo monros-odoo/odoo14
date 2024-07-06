eb6ea7e54c6eab970d982e7d3098d909ad0ef882
nithin <nithin@flintelli.com>
2024-07-06 15:40:35 +0530

    Revert "9823"
    This reverts commit b4b8654a832e652634558854014624cd01a580f1.

--- edit_invoice_standard_report/models/account_move.py
+++ edit_invoice_standard_report/models/account_move.py
@@ -6,12 +6,12 @@ from odoo import api, fields, models, _
 
 class AccountMoveline(models.Model):
     _inherit = 'account.move.line'
-
     total_before_discount = fields.Float(compute='compute_total_before_discount', string='Subtotal')
 
     @api.depends('price_unit', 'quantity')
     def compute_total_before_discount(self):
         for rec in self:
+
             rec.total_before_discount= rec.price_unit * rec.quantity
 
 
@@ -23,10 +23,10 @@ class AccountMove(models.Model):
     @api.depends('invoice_line_ids.price_unit','invoice_line_ids.quantity')
     def compute_total_before_discount(self):
         for rec in self:
+
             total_before_discount = 0.0
             for line in self.invoice_line_ids:
-                if line.price_unit >= 0:
-                    total_before_discount += line.price_unit * line.quantity
+                total_before_discount += line.price_unit * line.quantity
 
             rec.total_before_discount = total_before_discount
 
