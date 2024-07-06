ba6eb8b78d1193af0b2cf957c517b99e2fee35f7
nithin <nithin@flintelli.com>
2024-07-06 15:40:35 +0530

    Revert "9823"
    This reverts commit b4b8654a832e652634558854014624cd01a580f1.

--- sale_global_discounts/__manifest__.py
+++ sale_global_discounts/__manifest__.py
@@ -15,7 +15,7 @@
     Discount on Total
     Discount in Sale
     Global Discount in Sale""",
-    'depends': ['sale','account','sale_discount_total','edit_invoice_standard_report'],
+    'depends': ['sale','account'],
     'data': [
         'views/sale_view.xml',
         'views/account_invoice.xml',
--- sale_global_discounts/models/sale.py
+++ sale_global_discounts/models/sale.py
@@ -6,51 +6,19 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.depends('order_line.price_total')
-    def _amount_all(self):
-        """
-        Compute the total amounts of the SO.
-        """
-        for order in self:
-            amount_untaxed = amount_tax = amount_discount = 0.0
-            for line in order.order_line:
-                amount_untaxed += line.price_subtotal
-                amount_tax += line.price_tax
-            total = 0.0
-            if order.discount_types == 'fixed':
-                self.amount_discount = order.acs_discount_amount
-            if order.discount_types == 'percentage':
-                for line in order.order_line:
-                    total += line.price_subtotal
-                amount = (total * order.discount_percentage)/100
-                self.amount_discount = amount
-            order.update({
-                'amount_untaxed': amount_untaxed + self.amount_discount,
-                'amount_tax': amount_tax,
-                'amount_total': amount_untaxed + amount_tax,
-            })
-
     global_discount = fields.Boolean("Add Global Discount", readonly=True, states={'draft': [('readonly', False)]},)
     discount_types = fields.Selection([('fixed','Fixed'),('percentage','Percentage')],
         "Discount Type", readonly=True, states={'draft': [('readonly', False)]}, default='fixed')
     acs_discount_amount = fields.Float("Discount Amount", readonly=True, states={'draft': [('readonly', False)]},)
     discount_percentage = fields.Float("Discount Percentage", readonly=True, states={'draft': [('readonly', False)]},)
     total_discount_amount = fields.Float("Discount Amount", store=True)
-    amount_untaxed_dis = fields.Monetary(string='Untaxed Amount', store=True,
-                                         compute='_compute_amount_untaxed_dis',
-                                         track_visibility='always')
-    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
+    amount_untaxed_dis = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_compute_amount_untaxed_dis',
                                      track_visibility='always')
-    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
-                                 track_visibility='always')
-    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
-                                   track_visibility='always')
-    amount_discount = fields.Monetary(string='Discount', store=True, track_visibility='always')
 
-    @api.depends('total_discount_amount', 'amount_untaxed')
+    @api.depends('total_discount_amount','amount_untaxed')
     def _compute_amount_untaxed_dis(self):
         for rec in self:
-            rec.amount_untaxed_dis = rec.amount_untaxed + rec.total_discount_amount
+            rec.amount_untaxed_dis = rec.amount_untaxed+rec.total_discount_amount
 
     def _discount_unset(self):
         if self.env.user.company_id.sale_discount_product_id:
@@ -71,12 +39,12 @@ class SaleOrder(models.Model):
             total = 0.0
             if order.discount_types == 'fixed':
                 amount = order.acs_discount_amount
-                self.amount_discount = order.acs_discount_amount
+                self.total_discount_amount = order.acs_discount_amount
             if order.discount_types == 'percentage':
                 for line in order.order_line:
                     total += line.price_subtotal
                 amount = (total * order.discount_percentage)/100
-                self.amount_discount = amount
+                self.total_discount_amount = amount
 
             # Create the Sale line
             Line.create({
@@ -94,8 +62,7 @@ class SaleOrder(models.Model):
     def _prepare_invoice(self, ):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
         invoice_vals.update({
-            'total_discount': self.amount_discount,
-            'total_before_discount': self.amount_untaxed
-
+            'total_discount_amount': self.total_discount_amount,
+            'amount_untaxed_dis': self.amount_untaxed_dis,
         })
         return invoice_vals
