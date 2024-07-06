49648d1465bcd09d8aaf6d57baba750fd3c9d1d3
nithin <nithin@flintelli.com>
2024-07-06 15:40:00 +0530

    Revert "9823"
    This reverts commit 4eb74d213a4aa7c27d7673449c0fe465972b39c4.

--- account_discount_amount/models/move.py
+++ account_discount_amount/models/move.py
@@ -41,15 +41,13 @@ class AccountMove(models.Model):
             rec.amount_text = rec.currency_id.amount_to_text(rec.amount_total_signed)
 
 
-    @api.depends('invoice_line_ids')
+    @api.depends('invoice_line_ids','invoice_line_ids.total_discount')
     def get_tot_discount_amount(self):
         for rec in self:
             tot = 0.0
             for line in rec.invoice_line_ids:
-                if line.price_unit < 0:
-                    tot += -(line.quantity *line.price_unit)
+                tot += line.total_discount
             rec.total_discount = tot
-            print("rec.total_discount",rec.total_discount)
 
 
     @api.depends('invoice_line_ids','invoice_line_ids.quantity')
