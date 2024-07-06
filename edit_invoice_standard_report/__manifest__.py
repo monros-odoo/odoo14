2e114f5e66e5663cab77fd88421322faee991c3f
nithin <nithin@flintelli.com>
2024-07-06 15:40:57 +0530

    Revert "9823"
    This reverts commit 3b32f106b1f6c0cb36ccb40b8045e7f5f14aef01.

--- edit_invoice_standard_report/__manifest__.py
+++ edit_invoice_standard_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'edit invoice standard report',
     'author': "Marwa Ahmed",
-    'depends': ['base','account','sale_stock' ,'account_discount_amount','sale_global_discounts'],
+    'depends': ['base','account','sale_stock' ,'account_discount_amount'],
     'data': [
              'views/templates.xml',
 
